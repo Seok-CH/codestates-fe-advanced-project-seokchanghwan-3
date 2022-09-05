057390f1f1835d3b00b01dc1a3cbb8cbfa631907
Seok-CH <tjr6677@gmail.com>
2022-09-05 20:14:44 +0900

    Feat: calculator 컴포넌트 분리 및 새로 생성, 계산 로직 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
-import { pad } from "./pad";
-import { calculate } from "./calculate";
+import Calculator from "./components/Calculator";
+
 import { ReactComponent as Lightmode } from "./assets/images/lightmode.svg";
 import { ReactComponent as Darkmode } from "./assets/images/darkmode.svg";
 
-import { PadItemType } from "./pad";
-
 function App() {
   const [theme, setTheme] = useState("");
-  const [expression, setExpression] = useState("");
-  const [isClosedBracket, setIsClosedBracket] = useState(false);
-  const exDisplay = useRef() as React.MutableRefObject<HTMLElement>;
-
+  // TODO : localStorage에 저장된 다크모드 값을 가져와서 덮어쓰기
   const initializeTheme = (darkSchemeMediaQueryList: MediaQueryList) => {
     const theme = darkSchemeMediaQueryList.matches ? "dark" : "light";
     document.body.dataset.theme = theme;
@@ -25,108 +20,9 @@ function App() {
     setTheme(newTheme);
   };
 
-  const operateCalc = (el: PadItemType) => {
-    switch (el.type) {
-      case "reset":
-        resetCalc();
-        break;
-      case "undo":
-        deleteCommand();
-        break;
-      case "bracket":
-        addBracket();
-        break;
-      case "operator":
-        addCommand(el.command);
-        break;
-      case "number":
-        if (checkNumberOverflow()) break;
-        addCommand(el.command);
-        break;
-      case "changer":
-        changePlusMinus();
-        break;
-      case "equal":
-        makeResult(expression);
-        break;
-      default:
-    }
-  };
-
-  const resetCalc = () => {
-    setExpression("");
-  };
-
-  const addCommand = (command: string) => {
-    setExpression(expression + command);
-  };
-
-  const deleteCommand = () => {
-    setExpression(expression.slice(0, -1));
-  };
-
-  const addBracket = () => {
-    if (!isClosedBracket) {
-      setExpression(expression + "(");
-      setIsClosedBracket(true);
-    } else {
-      setExpression(expression + ")");
-      setIsClosedBracket(false);
-    }
-  };
-
-  const changePlusMinus = () => {
-    const recentNumRegExp = new RegExp(/-?[0-9.]+$/);
-    const recentNum = expression.match(recentNumRegExp);
-    if (!recentNum) return;
-
-    const changeNum = +recentNum[0] > 0 ? -+recentNum[0] : Math.abs(+recentNum[0]);
-    const newExpression = expression.replace(recentNumRegExp, `${changeNum}`);
-    setExpression(newExpression);
-  };
-
-  const checkNumberOverflow = () => {
-    const recentNumRegExp = new RegExp(/[0-9.]+$/);
-    const recentNum = expression.match(recentNumRegExp);
-    if (!recentNum) return false;
-    if (recentNum[0].length >= 12) return true;
-  };
-
-  const makeResult = (expression: string) => {
-    setExpression(calculate(expression));
-  };
-
-  const formatExpression = (raw: string) => {
-    return raw
-      .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")
-      .replace(/\+/g, " + ")
-      .replace(/\*/g, " * ")
-      .replace(/\//g, " / ")
-      .replace(/-/g, " - ");
-  };
-
-  const changeFontSize = () => {
-    const pNode = exDisplay.current.parentNode as HTMLElement;
-    const pWidth = pNode.offsetWidth;
-    const cWidth = exDisplay.current.offsetWidth;
-    const cFontSize = +window.getComputedStyle(exDisplay.current).fontSize.slice(0, -2);
-
-    if (cWidth >= pWidth * 0.95 && cFontSize > 14) {
-      exDisplay.current.style.fontSize = `${cFontSize - 8}px`;
-    }
-
-    if (cWidth < pWidth * 0.5 && cFontSize < 30) {
-      exDisplay.current.style.fontSize = `${cFontSize + 8}px`;
-    }
-  };
-
   useEffect(() => {
     initializeTheme(window.matchMedia("(prefers-color-scheme: dark)"));
   }, []);
-
-  useEffect(() => {
-    changeFontSize();
-  }, [exDisplay.current?.offsetWidth]);
 
   return (
     <div className="App">
@@ -136,24 +32,7 @@ function App() {
           {theme === "dark" ? <Darkmode /> : <Lightmode />}
         </button>
       </header>
-      <main className="calc">
-        <section className="calc__display">
-          <span ref={exDisplay}>{formatExpression(expression)}</span>
-        </section>
-        <section className="calc__pad">
-          {pad.map((el) => (
-            <div
-              className="calc__paditem"
-              key={el.command}
-              onClick={() => {
-                operateCalc(el);
-              }}
-            >
-              {el.command}
-            </div>
-          ))}
-        </section>
-      </main>
+      <Calculator />
     </div>
   );
 }
